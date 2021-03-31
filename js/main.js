7af8110fe95463e7f407510c3b0516c37b0fbaf2
NeskoDorc <nebojsa85.milosevic@gmail.comm>
2021-03-31 13:15:57 +0200

    fin2

--- js/main.js
+++ js/main.js
@@ -8,16 +8,22 @@ window.onscroll = function() {myFunction()};
 
 // Get the header
 var header = document.getElementById("myHeader");
+var header2 = document.getElementById("myHeader2");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
+var sticky = header2.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    header2.classList.add("sticky");
+    header2.classList.add("po");
   } else {
     header.classList.remove("sticky");
+    header2.classList.remove("sticky");
+    header2.classList.remove("po");
   }
 }
 
